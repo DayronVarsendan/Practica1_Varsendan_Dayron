7bae47a957ee6a3b9aa6f3ee0935efd71d553ac7
DayronVarsendan <dayronvarsendan@gmail.com>
2025-10-07 19:00:50 +0200

    Commit 3
    Ejemplo de texto en rama_secundaria1

---
+++ src/Package_practica1/Ejemplo.java
@@ -0,0 +1,14 @@
+package Package_practica1;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("Hola rama 1");
+
+	}
+
+}
